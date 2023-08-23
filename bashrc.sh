f1789c5555ea1842866536a272f61b0015d481c7
barwe <995488247@qq.com>
2023-08-23 09:50:03 +0800

    update

--- bashrc.sh
+++ bashrc.sh
@@ -1,2 +1,2 @@
-source $HOME/bashrc.d/alias-core.sh
-source $HOME/bashrc.d/ps1.sh
+. $HOME/bashrc.d/alias-core.sh
+. $HOME/bashrc.d/ps1.sh
--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # wget https://raw.githubusercontent.com/barwe/bashrc.d/master/install.sh && bash install.sh
+
+[ -d "$HOME/bashrc.d" ] && rm -rf $HOME/bashrc.d
 git clone --depth 1 https://github.com/barwe/bashrc.d.git $HOME/bashrc.d
 
 if [ -z "$(grep BARWE_BASHRC_D $HOME/.bashrc)" ]; then
-  echo -e '## BARWE_BASHRC_D' >> $HOME/.bashrc;
+  echo -e '\n\n## BARWE_BASHRC_D' >> $HOME/.bashrc;
   echo -e '[ -d $HOME/bashrc.d ] && source $HOME/bashrc.d/bashrc.sh' >> $HOME/.bashrc;
 fi
 
-source $HOME/bashrc.d/bashrc.sh
+. $HOME/bashrc.d/bashrc.sh
