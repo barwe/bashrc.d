4c2d276d76053820333f4445a40ac1f66f5c3925
barwe <995488247@qq.com>
2023-08-23 09:55:50 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# https://dnschecker.org/country/cn/#A/raw.githubusercontent.com
+# sudo echo "185.199.110.133 raw.githubusercontent.com" >> /etc/hosts
 # wget https://raw.githubusercontent.com/barwe/bashrc.d/master/install.sh && bash install.sh
 
 [ -d "$HOME/bashrc.d" ] && rm -rf $HOME/bashrc.d
